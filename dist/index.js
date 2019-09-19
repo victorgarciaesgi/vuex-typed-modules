2ff6c7978101f178c8c5511669f44358a2739f7a
Victor GARCIA <victor@totem.paris>
2019-09-19 11:58:29 +0200

    fixed gitignore

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Database_1 = require("./Database");
-exports.Database = Database_1.Database;
-var Module_1 = require("./Module");
-exports.VuexModule = Module_1.VuexModule;
