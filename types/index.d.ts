017f2514f67422d2f02f8064fe9ddafc73efd45c
Victor Garcia <victor@totem.paris>
2021-05-21 22:23:48 +0200

    fixed export

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
 export { Database } from './database';
 export { VuexModule, VuexModuleArgs, createVuexModule } from './modules/default';
 export { VuexModuleHook } from './modules/hooks';
-export { VuexDynamicModule, ModuleToInstance } from './modules/dynamic';
+export { VuexDynamicModule, ModuleToInstance, createVuexDynamicModule } from './modules/dynamic';
